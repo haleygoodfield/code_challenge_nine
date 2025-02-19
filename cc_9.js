67a3ea37dc3d19592f7615e6b004c98e06990fca
Haley Goodfield <haleygoodfield@usf.edu>
2025-02-18 19:50:18 -0500

    Task 2 - Created Manager Class with Inheritance

--- cc_9.js
+++ cc_9.js
@@ -1,15 +1,15 @@
 // Task 1: Creating an Employee Class
 // Create a class Employee with the following properties: name, id, department, salary
 class Employee {
-    constructor(name, id, deparmtent, salary) {
+    constructor(name, id, department, salary) {
         this.name = name;
         this.id = id;
-        this.deparmtent = deparmtent;
+        this.department = department;
         this.salary = salary;
     } 
     // Add a method getDetails() that returns a formatted string of employee details
     getDetails () {
-        return `Employee name: ${this.name}, ID: ${this.id}, Department: ${this.deparmtent}, Salary: $${this.salary}`;
+        return `Employee name: ${this.name}, ID: ${this.id}, Department: ${this.department}, Salary: $${this.salary}`;
     }
     // Add a method calculateAnnualSalary() that returns the employee’s annual salary (salary * 12)
     calculateAnnualSalary() {
@@ -23,3 +23,28 @@ console.log(emp1.getDetails());
 // Expected output: "Employee: Alice Johnson, ID: 101, Department: Sales, Salary: $5000"
 
 console.log(emp1.calculateAnnualSalary()); // Expected output: 60000
+
+
+
+// Task 2: Creating a Manager Class (Inheritance & Method Overriding)
+class Manager extends Employee { // The Manager class should inherit from Employee
+    constructor(name, id, department, salary, teamSize) { // Add a new property: teamSize (number)
+        super(name, id, department, salary);
+        this.teamSize = teamSize;
+    }
+    // Override getDetails() to include the team size
+    getDetails() {
+        return `Manager: $${this.name}, ID: ${this.id}, Department: ${this.department}, Salary: $${this.salary}, Team Size: ${this.teamSize}`;
+    }
+    // Add a method calculateBonus() that returns 10% of the manager’s annual salary
+    calculateBonus() {
+        return this.calculateAnnualSalary() * 0.1;
+    }
+}
+// Test Cases 
+const mgr1 = new Manager("John Smith", 201, "IT", 8000, 5);
+console.log(mgr1.getDetails());
+// Expected output: "Manager: John Smith, ID: 201, Department: IT, Salary: $8000, Team Size: 5"
+
+console.log(mgr1.calculateBonus()); // Expected output: 9600
+
